ff45c58207af974df571bb315cadc6d9a9d64617
Dell <ponikpadadhariya309@gmail.com>
2024-04-29 22:02:49 +0530

    change

---
+++ android/app/src/main/kotlin/com/example/platform_change_contact/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.platform_change_contact
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp],);
+  runApp(
+      MaterialApp(
+
+      )
+  );
+}
