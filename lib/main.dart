0f67f9649aa4078ad4d4f0ad0987b3af88021fb7
Dell <ponikpadadhariya309@gmail.com>
2024-04-30 12:06:05 +0530

    30

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:platform_change_contact/utils/screen_routes.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp],);
+  SystemChrome.setPreferredOrientations(
+    [DeviceOrientation.portraitUp],
+  );
   runApp(
-      MaterialApp(
-
-      )
+    MaterialApp(
+      routes: screen,
+    ),
   );
 }
---
+++ lib/utils/screen_routes.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+import '../screen/home/home_screen.dart';
+
+Map<String, WidgetBuilder> screen = {
+  '/': (context) => const HomeScreen(),
+};
